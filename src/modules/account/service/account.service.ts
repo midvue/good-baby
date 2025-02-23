f4a3b8bceebfdc7050ab26fbad0951817f9cccd9
spig <zybieku@gmail.com>
2025-02-24 00:12:20 +0800

    feat:添加account的service

--- src/modules/account/service/account.service.ts
+++ src/modules/account/service/account.service.ts
@@ -1,7 +1,6 @@
-import { Inject, Provide, httpError } from '@midwayjs/core';
-import { JwtService } from '@midwayjs/jwt';
-import { InjectEntityModel, TypeORMDataSourceManager } from '@midwayjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { Provide } from '@midwayjs/core';
+import { InjectEntityModel } from '@midwayjs/typeorm';
+import { Like, Repository } from 'typeorm';
 import { BaseService } from '../../base/base.service';
 import {
   AccountCreateDTO,
@@ -13,151 +12,45 @@ import { Account } from '../entity/account';
 @Provide()
 export class AccountService extends BaseService {
   @InjectEntityModel(Account)
-  userModel: Repository<Account>;
-
-  @Inject()
-  jwtService: JwtService;
-
-  @Inject()
-  dataSourceMgr: TypeORMDataSourceManager;
+  accountModel: Repository<Account>;
 
   async page(options: Partial<AccountPageDTO>) {
-    const { id, account } = options;
+    const { id, name, code } = options;
 
-    let qBuilder = this.userModel.createQueryBuilder('a').select(
-      ` 
-        a.id,
-        a.account,
-        a.phone,
-        a.status,
-        a.remark,
-        a.gender,
-        a.nickname,
-        a.password,
-        a.dept_id AS deptId,
-        a.create_time AS createTime,
-        a.update_time AS updateTime,
-        `
-    );
-    if (id) {
-      qBuilder = qBuilder.where('a.id = :id', { id });
-    }
-    if (account) {
-      qBuilder = qBuilder.where('a.account like :account', {
-        account: account + '%',
-      });
-    }
-    qBuilder = qBuilder
-      .groupBy('a.id')
-      .orderBy({
-        'a.id': 'ASC',
-      })
-      .skip((options.current - 1) * options.size)
-      .take(options.size);
+    const where = {
+      id,
+      code,
+      name: name ? Like(name + '%') : undefined,
+    };
 
-    const [list, count] = await Promise.all([
-      qBuilder.getRawMany(),
-      qBuilder.getCount(),
-    ]);
+    const [list, count] = await this.accountModel.findAndCount({
+      where,
+      order: {
+        id: 'DESC',
+        createTime: 'DESC',
+      },
+      skip: (options.current - 1) * options.size,
+      take: options.size,
+    });
+
     return { list, count };
   }
 
   async info(id: number) {
-    if (!id) return {};
-    const findUse = this.userModel.findOne({
-      select: [
-        'id',
-        'account',
-        'nickname',
-        'phone',
-        'deptId',
-        'status',
-        'gender',
-        'createTime',
-        'updateTime',
-      ],
-      where: { id },
-    });
-
-    const findRoles = this.roleModel
-      .createQueryBuilder('role')
-      .select('role.id as roleId,role.name as roleName')
-      .where(qb => {
-        const subQuery = qb
-          .subQuery()
-          .select('ur.role_id')
-          .from(AccountRole, 'ur')
-          .where('ur.user_id = :uid', { uid: id })
-          .getQuery();
-        return 'role.id IN ' + subQuery;
-      });
-
-    const [info, roleList] = await Promise.all([
-      findUse,
-      findRoles.getRawMany(),
-    ]);
-    return { ...info, roles: roleList };
+    const info = await this.accountModel.findOne({ where: { id } });
+    return info;
   }
 
   async create(inDto: AccountCreateDTO) {
-    const { roleIds, ...user } = inDto;
-
-    const dataSource = this.dataSourceMgr.getDataSource('default');
-    const res = await dataSource.transaction(async transMgr => {
-      //事务中插入
-      const _user = await transMgr.save(Account, user);
-      const userRoleModels = roleIds.map(rid => ({
-        roleId: rid,
-        userId: _user.id,
-      }));
-      return await transMgr.save(AccountRole, userRoleModels);
-    });
-    return res?.[0];
+    const { id } = await this.accountModel.save(inDto);
+    return { id };
   }
 
-  async update(dto: AccountUpdateDTO) {
-    const { roleIds, ...user } = dto;
-
-    //查询库里面绑定的roleIds
-    const urIds = await this.userRoleModel
-      .find({
-        select: ['roleId'],
-        where: { userId: user.id },
-      })
-      .then(_uRoles => _uRoles.map(ur => ur.roleId));
-
-    const dataSource = this.dataSourceMgr.getDataSource('default');
-    return await dataSource.transaction(async transMgr => {
-      await transMgr.update(Account, user.id, user);
-
-      for (const rid of roleIds) {
-        //如果数据库没有对应的角色,就添加
-        const index = urIds.indexOf(rid);
-        if (index === -1) {
-          transMgr.insert(AccountRole, { userId: user.id, roleId: rid });
-        } else {
-          //过滤掉数据库已经有的
-          urIds.splice(index, 1);
-        }
-      }
-      //删除库里冗余的角色
-      await transMgr.delete(AccountRole, {
-        userId: user.id,
-        roleId: In(urIds),
-      });
-    });
+  async update(upDto: AccountUpdateDTO) {
+    return await this.accountModel.update(upDto.id, upDto);
   }
 
   async delete(id: number) {
-    return await this.userModel.delete(id);
-  }
-
-  async getAccountByAccount(account: string) {
-    if (!account) throw new httpError.ForbiddenError('username不能为空');
-    const user = await this.userModel.findOne({
-      select: ['id', 'deptId', 'status', 'password'],
-      where: { account },
-    });
-    return user || ({} as Account);
+    return await this.accountModel.delete(id);
   }
 }
